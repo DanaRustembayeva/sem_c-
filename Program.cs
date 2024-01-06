d3c2cbda1c53bfcca371ee24c4bc3e8169617a1b
Ваше Имя <@.com>
2024-01-06 20:28:21 +0600

    added second task

--- Program.cs
+++ Program.cs
@@ -16,15 +16,38 @@ a = -3, b = 9 => нет
 Console.Write("Введите первое число: "); //a
 int firstNumber = Convert.ToInt32(Console.ReadLine());
 
-Console.Write("Введите первое число: "); //b
+Console.Write("Введите второе число: "); //b
 int secondNumber = Convert.ToInt32(Console.ReadLine());
 
-if(firstNumber == secondNumber*secondNumber) // true
+if (firstNumber == secondNumber * secondNumber) // true
 {
-Console.WriteLine("Да" + firstNumber + "является кв. от числа" + secondNumber);
+    Console.WriteLine("Да" + firstNumber + "является кв. от числа" + secondNumber);
 }
 else // а не яляется кв от числа b
 {
-Console.WriteLine("НЕТ" + firstNumber + "НЕ является кв. от числа" + secondNumber);
+    Console.WriteLine("НЕТ" + firstNumber + "НЕ является кв. от числа" + secondNumber);
 }
 
+// on Windows: shift+Alt+F
+
+// Напишите программу, которая на вход принимает
+// целое число N, а на выходе показывает все целые
+// числа в промежутке от -N до N.
+// Примеры
+// 4 => -4, -3, -2, -1, 0, 1, 2, 3, 4
+// 2 => -2, -1, 0, 1, 2
+
+Console.Write("Введите число N: "); 
+int N = Convert.ToInt32(Console.ReadLine ()); //N=4
+if (N<0) //-5*(-1)=5
+{
+    Console.WriteLine("ВВедено отр.число N");
+    N = N * -1;
+}
+int negativeN = N * -1; //-4
+
+while (negativeN <= N)// -3-2-10123
+{
+    Console.Write(negativeN + " ");
+    negativeN++; // negativeN++ (negativeN = negativeN + 1)
+}
